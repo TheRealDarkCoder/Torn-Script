19e69948db0d15f64954091edb6af42a43761bf0
DeKleineKobini <kobedepeuter@outlook.com>
2019-09-11 12:20:15 +0200

    kill script due to bug

--- eliminationbot.user.js
+++ eliminationbot.user.js
@@ -1,34 +1,11 @@
 // ==UserScript==
-// @name           TORN: Elimination Bot
-// @version        1.0.0
+// @name           TORN: Elimination Bot Scraper
+// @version        1.0.1
 // @namespace      http://darkcoder.xyz
-// @match          https://www.torn.com/profiles.php*
+// @match          https://www.torn.com/competition.php*
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require        https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @downloadURL    https://github.com/DeKleineKobini/Torn-Script/raw/master/eliminationbot.user.js
 // @grant          GM_xmlhttpRequest
 // ==/UserScript==
 
-waitForKeyElements ("#competition-profile-wrapper > div > div.cont.bottom-round > div > span > div.block-info > div.score", actionFunction);
-
-function actionFunction (jNode) {
-    score = document.querySelector("#competition-profile-wrapper > div > div.cont.bottom-round > div > span > div.block-info > div.score").innerText;
-    position = document.querySelector("#competition-profile-wrapper > div > div.cont.bottom-round > div > span > div.block-info > div.position").innerText;
-    last_updated = document.querySelector("#sidebar > div:nth-child(4) > div > div.footer-menu___24_mz.left___3fJFS > div.date___1fSgP > span:nth-child(2)").innerText;
-    team = document.querySelector("#competition-profile-wrapper > div > div.cont.bottom-round > div > span > div.block-info > div.sigil.trolls").className;
-    scores = {score : score, position: position, last_updated: last_updated};
-    console.log(scores);
-    if(team == "sigil trolls") {
-        GM_xmlhttpRequest ({
-            method:     "POST",
-            url:        "http://darkcoder.pythonanywhere.com",
-            data:       JSON.stringify ( scores ),
-            headers:    {
-                "Content-Type": "application/json"
-            },
-            onload:     function (response) {
-                console.log ("POSTED");
-            }
-        });
-    }
-}
