59bf5ba26fefe6b1040fc080eb1439bc309f66bc
DeKleineKobini <kobedepeuter@outlook.com>
2019-09-12 10:08:35 +0200

    2.3.0 time

--- eliminationbot.user.js
+++ eliminationbot.user.js
@@ -1,65 +1,68 @@
 // ==UserScript==
 // @name           TORN: Elimination Bot Scraper
-// @version        2.2.0
+// @version        2.3.0
 // @namespace      http://darkcoder.xyz
 // @match          https://www.torn.com/competition.php
 // @downloadURL    https://github.com/DeKleineKobini/Torn-Script/raw/master/eliminationbot.user.js
 // @grant          GM_xmlhttpRequest
 // ==/UserScript==
 
-setInterval (function () {
+setInterval (function() {
     var teamNames = ['lumberjacks', 'field-mice', 'trolls', 'millennials', 'dream-team', 'breakfast-club', 'band-of-mothers', 'hillbillies',
                      'revengers', 'punchbags', 'cowboys', 'keyboard-warriors'];
-    var data = [
-        {
-            'name': 'lumberjacks',
-            'score': "Waiting for data"
-        },
-        {
-            'name': 'field-mice',
-            'score': "Waiting for data"
-        },
-        {
-            'name': 'trolls',
-            'score': "Waiting for data"
-        },
-        {
-            'name': 'millennials',
-            'score': "Waiting for data"
-        },
-        {
-            'name': 'dream-team',
-            'score': "Waiting for data"
-        },
-        {
-            'name': 'breakfast-club',
-            'score': "Waiting for data"
-        },
-        {
-            'name': 'band-of-mothers',
-            'score': "Waiting for data"
-        },
-        {
-            'name': 'hillbillies',
-            'score': "Waiting for data"
-        },
-        {
-            'name': 'revengers',
-            'score': "Waiting for data"
-        },
-        {
-            'name': 'punchbags',
-            'score': "Waiting for data"
-        },
-        {
-            'name': 'cowboys',
-            'score': "Waiting for data"
-        },
-        {
-            'name': 'keyboard-warriors',
-            'score': "Waiting for data"
-        },
-    ]
+    var data = {
+        "time" : "Waiting for data",
+        "teams" : [
+            {
+                'name': 'lumberjacks',
+                'score': "Waiting for data"
+            },
+            {
+                'name': 'field-mice',
+                'score': "Waiting for data"
+            },
+            {
+                'name': 'trolls',
+                'score': "Waiting for data"
+            },
+            {
+                'name': 'millennials',
+                'score': "Waiting for data"
+            },
+            {
+                'name': 'dream-team',
+                'score': "Waiting for data"
+            },
+            {
+                'name': 'breakfast-club',
+                'score': "Waiting for data"
+            },
+            {
+                'name': 'band-of-mothers',
+                'score': "Waiting for data"
+            },
+            {
+                'name': 'hillbillies',
+                'score': "Waiting for data"
+            },
+            {
+                'name': 'revengers',
+                'score': "Waiting for data"
+            },
+            {
+                'name': 'punchbags',
+                'score': "Waiting for data"
+            },
+            {
+                'name': 'cowboys',
+                'score': "Waiting for data"
+            },
+            {
+                'name': 'keyboard-warriors',
+                'score': "Waiting for data"
+            }
+        ]
+    };
 
     for(let i = 0; i < teamNames.length; i++){
         let teamName = teamNames[i];
@@ -67,25 +70,34 @@ setInterval (function () {
         let score = document.querySelector("#" + teamNames[i] + " > ul > li.score > span");
         let lives = document.querySelector("#" + teamNames[i] + " > ul > li.lives");
         if (score) {
-            for (var k in data) {
-                if (data[k].name == teamName) {
-                    data[k].score = parseInt(score.innerText.replace(',', ""));
-                    data[k].lives = parseInt(lives.innerText);
+            for (let k in data.teams) {
+                if (data.teams[k].name == teamName) {
+                    data.teams[k].score = parseInt(score.innerText.replace(',', ""));
+                    data.teams[k].lives = parseInt(lives.innerText);
                 }
             }
         }
     }
 
-    GM_xmlhttpRequest ({
-        method:     "POST",
-        url:        "http://darkcoder.pythonanywhere.com/api/",
-        data:       JSON.stringify ( data ),
-        headers:    {
-            "Content-Type": "application/json"
-        },
-        onload:     function (response) {
-            console.log ("POSTED");
-            console.log(data);
-        }
-    });
+    let time = document.querySelector("#competition-wrap > div.competition-wrap.header-competition-wrap.m-bottom10 > div.elimination-image.trolls.sigil > div > span");
+    data.time = time.innerText;
+
+    var sendData = function() {
+        GM_xmlhttpRequest ({
+            method:     "POST",
+            url:        "http://darkcoder.pythonanywhere.com/api/",
+            data:       JSON.stringify (data),
+            headers:    {
+                "Content-Type": "application/json"
+            },
+            onload:     function (response) {
+                console.log ("POSTED");
+                console.log(data);
+            }
+        });
+    };
+
+    if (data.teams[11].score != "Waiting for data"){
+        sendData();
+    }
 }, 10000);
